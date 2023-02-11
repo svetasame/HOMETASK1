b965d9530d6a324b726c09ed7550fa7484a93854
svetlana <milkshake9273@gmai.com>
2023-02-11 21:22:37 +1000

    find max - three int

--- task4/Program.cs
+++ task4/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int [] {a, b, c};
+int i = 0;
+int max = numbers [i];
+while(i < numbers.Length)
+{
+  if (numbers[i] > max)
+  {
+    max = numbers[i];
+  }
+  else
+  {
+    i++;
+  }
+}
+Console.Write("Максимальное число из трех: ");
+Console.Write(max);
