8ff37ad1c6977c8bc8d42bc48b81a3398b0eba72
svetlana <milkshake9273@gmai.com>
2023-02-11 21:43:39 +1000

    every even numbers

--- task8/Program.cs
+++ task8/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+for (int i = 1; i<=N; i++)
+{
+    if (i%2 == 0)
+    {
+     Console.Write(i);
+     Console.Write(", ");
+    }
+}
