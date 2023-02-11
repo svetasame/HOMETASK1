1d12ff9cb1135afa013baf539bc9ba76bc80e4eb
svetlana <milkshake9273@gmai.com>
2023-02-11 21:28:36 +1000

    find even odd

--- task6/Program.cs
+++ task6/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine ("Введите целое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a%2 == 0)
+{
+  Console.WriteLine ("ДА");
+}
+else
+{
+  Console.WriteLine ("НЕТ");
+}
