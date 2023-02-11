bccfe62c9e72504ccf4d2256e7aea667082730ed
svetlana <milkshake9273@gmai.com>
2023-02-11 20:32:27 +1000

    find max - two int

---
+++ task2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a > b)
+{
+  Console.Write("max = ");
+  Console.Write(a);
+}
+else
+{
+  Console.Write("max = ");
+  Console.Write(b);
+}
